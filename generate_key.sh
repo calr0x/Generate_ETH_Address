23cfd2ed9f14cefcd838c34cbd22920879bfe02b
calvin <calr0x@gmail.com>
2022-01-08 09:27:37 -0600

    .

--- generate_key.sh
+++ generate_key.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 N1=$'\n'
 
-rm key pub priv address
-
 KEY=$(openssl ecparam -name secp256k1 -genkey -noout | openssl ec -text -noout)
 
-echo "${N1}$KEY" > key
+PUB=$(echo "$KEY" | grep pub -A 5 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^04//')
 
-PUB=$(cat key | grep pub -A 5 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^04//')
+PRIV=$(echo "$KEY" | grep priv -A 3 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^00//')
 
-echo "$PUB" > pub
-
-PRIV=$(cat key | grep priv -A 3 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^00//')
-
-echo "$PRIV" > priv
-
-ADDRESS=$(cat pub | keccak-256sum -x -l | tr -d ' -' | tail -c 41)
-
-echo "$ADDRESS" > address
+ADDRESS=$(echo "$PUB" | keccak-256sum -x -l | tr -d ' -' | tail -c 41)
 
 clear
 echo "${N1}Public Key  = 0x$ADDRESS"
