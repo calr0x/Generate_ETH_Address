445c3785073bd82dfd3aca23c4f3faf2989d7178
calvin <calr0x@gmail.com>
2022-03-06 07:40:30 -0600

    .

--- generate_key.sh
+++ generate_key.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 N1=$'\n'
 
-KEY=$(openssl ecparam -name secp256k1 -genkey -noout | openssl ec -text -noout)
-
-PUB=$(echo "$KEY" | grep pub -A 5 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^04//')
-
-PRIV=$(echo "$KEY" | grep priv -A 3 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^00//')
-
-ADDRESS=$(echo "$PUB" | sha256sum | tr -d ' -' | tail -c 41)
-
 clear
 
 if [[ -f /root/eth_keys ]]; then
@@ -25,6 +17,11 @@ i=1
 
 while [[ $i -le $TOTAL ]]
 do
+    KEY=$(openssl ecparam -name secp256k1 -genkey -noout | openssl ec -text -noout)
+    PUB=$(echo "$KEY" | grep pub -A 5 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^04//')
+    PRIV=$(echo "$KEY" | grep priv -A 3 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^00//')
+    ADDRESS=$(echo "$PUB" | sha256sum | tr -d ' -' | tail -c 41)
+    
     echo "Key $i:"
     echo "${N1}Public Key  = 0x$ADDRESS"
     echo "${N1}Public Key  = 0x$ADDRESS" >> /root/eth_keys
