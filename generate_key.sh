b9f6f1996fe5f732ea460895471a2a0ad9cc5ff8
calvin <calr0x@gmail.com>
2022-03-07 10:44:57 -0600

    .

--- generate_key.sh
+++ generate_key.sh
@@ -22,7 +22,8 @@ do
     KEY=$(openssl ecparam -name secp256k1 -genkey -noout | openssl ec -text -noout)
     PUB=$(echo "$KEY" | grep pub -A 5 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^04//')
     PRIV=$(echo "$KEY" | grep priv -A 3 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^00//')
-    ADDRESS=$(echo "$PUB" | sha256sum | tr -d ' -' | tail -c 41)
+#    ADDRESS=$(echo "$PUB" | sha256sum | tr -d ' -' | tail -c 41)
+    ADDRESS=$(echo "$PUB" | keccak-256sum -x -l | tr -d ' -' | tail -c 41)
 
     echo "${N1}${N1}Key $i:" >> /root/eth_keys
 #    echo "${N1}Public Key  = 0x$ADDRESS"
