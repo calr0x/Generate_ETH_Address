d36937aba2312b507b2f7b9cc3f8fa06ec6e698c
calvin <calr0x@gmail.com>
2022-03-07 11:01:19 -0600

    .

--- generate_key.sh
+++ generate_key.sh
@@ -24,7 +24,7 @@ echo "Total Keys: $TOTAL" >> /root/eth_keys
 
 while [[ $i -le $TOTAL ]]
 do
-    KEY=$(openssl ecparam -name secp256k1 -genkey -noout | openssl ec -text -noout)
+    KEY=$(openssl ecparam -name secp256k1 -genkey -noout | openssl ec -text -noout 2>/dev/null)
     PUB=$(echo "$KEY" | grep pub -A 5 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^04//')
     PRIV=$(echo "$KEY" | grep priv -A 3 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^00//')
 #    ADDRESS=$(echo "$PUB" | sha256sum | tr -d ' -' | tail -c 41)
